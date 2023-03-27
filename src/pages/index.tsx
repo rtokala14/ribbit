5e7f43d9d6961c865d2efb724dee67b79b4521b3
Rohit Tokala <rtokala@purdue.edu>
2023-03-27 19:28:29 -0400

    Fix lucide error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import { type NextPage } from "next";
 import Image from "next/image";
 import {
-  HomeIcon,
+  Home as HomeIcon,
   Search,
-  BellIcon,
-  BookmarkIcon,
-  UserIcon,
+  Bell,
+  Bookmark,
+  User,
   PlusCircle,
   MoreHorizontal,
 } from "lucide-react";
 import Link from "next/link";
+
+import Logo from "../../public/Logo.webp";
 
 const Home: NextPage = () => {
   return (
@@ -32,7 +34,7 @@ export function Sidebar() {
   return (
     <div className=" flex flex-col items-center gap-4 lg:items-start">
       <Link href={"/"}>
-        <Image alt="Ribbit logo" src="/Logo.webp" width={80} height={80} />
+        <Image alt="Ribbit logo" src={Logo} width={80} height={80} />
       </Link>
       <Link
         href={"/"}
@@ -52,29 +54,44 @@ export function Sidebar() {
         href={"/"}
         className="flex items-center gap-2 rounded-full p-2 hover:bg-base-200 lg:rounded-lg"
       >
-        <BellIcon className=" h-8 w-8" />
+        <Bell className=" h-8 w-8" />
         <h3 className="hidden lg:block">Notifications</h3>
       </Link>
       <Link
         href={"/"}
         className="flex items-center gap-2 rounded-full p-2 hover:bg-base-200 lg:rounded-lg"
       >
-        <BookmarkIcon className=" h-8 w-8" />
+        <Bookmark className=" h-8 w-8" />
         <h3 className="hidden lg:block">Bookmarks</h3>
       </Link>
       <Link
         href={"/"}
         className="flex items-center gap-2 rounded-full p-2 hover:bg-base-200 lg:rounded-lg"
       >
-        <UserIcon className=" h-8 w-8" />
+        <User className=" h-8 w-8" />
         <h3 className="hidden lg:block">Profile</h3>
       </Link>
       <Link
         href={"/"}
-        className="flex items-center gap-2 rounded-full p-2 hover:bg-base-200 lg:rounded-lg"
+        className="dropdown-right dropdown flex items-center gap-2 rounded-full p-2 hover:bg-base-200 lg:rounded-lg"
       >
-        <MoreHorizontal className=" h-8 w-8" />
-        <h3 className="hidden lg:block">Settings</h3>
+        <div>
+          <MoreHorizontal className=" h-8 w-8 " />
+          <h3 className="hidden lg:block">Settings</h3>
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu rounded-box w-52 bg-base-100 p-4 shadow"
+        >
+          <li>
+            <div>
+              <label className="label cursor-pointer">
+                <span className="label-text">Dark Mode</span>
+                <input type="checkbox" className="toggle" checked />
+              </label>
+            </div>
+          </li>
+        </ul>
       </Link>
       <Link
         href={"/"}
