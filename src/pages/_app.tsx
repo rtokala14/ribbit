89d1dd30f01ca7b54467e45f75b663918dc97c35
Rohit Tokala <rtokala@purdue.edu>
2023-03-27 18:31:32 -0400

    Add super basic layout and favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,25 @@ const MyApp: AppType<{ session: Session | null }> = ({
           content="A full stack clone of Twitter by Rohit Tokala"
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="shortcut icon" href="/favicon.ico"></link>
       </Head>
       <Component {...pageProps} />
     </SessionProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
 import { type NextPage } from "next";
-import ThemeToggle from "~/components/ThemeToggle";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <main className="">
-        <div>Ribbit</div>
-        <ThemeToggle />
-      </main>
-    </>
+    <main className="flex min-h-screen justify-center gap-4">
+      {/* Sidebar */}
+      <Sidebar />
+      <div className="min-h-screen w-full border-x border-neutral-content md:max-w-2xl lg:max-w-3xl">
+        {/* Timeline */}
+        <Timeline />
+      </div>
+      {/* Right Sidebar */}
+      <RightSidebar />
+    </main>
   );
 };
 
 export default Home;
+
+export function Sidebar() {
+  return <div className=" flex flex-col gap-2">Sidebar</div>;
+}
+
+export function Timeline() {
+  return <div>Timeline</div>;
+}
+
+export function RightSidebar() {
+  return <div className="hidden lg:flex">Right Sidebar</div>;
+}
