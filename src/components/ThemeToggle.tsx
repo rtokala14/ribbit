7f2118737db9f3526e3a5b1cb40fdf87812a0cdb
Rohit Tokala <rtokala@purdue.edu>
2023-03-28 21:05:43 -0400

    Persist theme in localStorage

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,12 +4,43 @@ import { useEffect, useState } from "react";
 
 function ThemeToggle() {
   const [theme, setTheme] = useState("fantasy");
+
   useEffect(() => {
-    document.querySelector("html")?.setAttribute("data-theme", theme);
+    // document.querySelector("html")?.setAttribute("data-theme", theme);
+    themeCheck();
   }, [theme]);
+
+  useEffect(() => {
+    themeCheck();
+  }, []);
+
+  const themeCheck = () => {
+    if (
+      localStorage.getItem("ribbit-theme") === "halloween" ||
+      (!("ribbit-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.querySelector("html")?.setAttribute("data-theme", "halloween");
+      setTheme("halloween");
+    } else {
+      document.querySelector("html")?.setAttribute("data-theme", "fantasy");
+      setTheme("fantasy");
+    }
+  };
+
   const toggleTheme = () => {
+    const localTheme = localStorage.getItem("ribbit-theme");
+    if (localTheme) {
+      localStorage.setItem(
+        "ribbit-theme",
+        localTheme === "halloween" ? "fantasy" : "halloween"
+      );
+    } else {
+      localStorage.setItem("ribbit-theme", "halloween");
+    }
     setTheme((theme) => (theme === "fantasy" ? "halloween" : "fantasy"));
   };
+
   return (
     <label className="label flex cursor-pointer items-center justify-between">
       <span className="label-text">Dark Theme</span>
